6b03be4e48883c2232fdec6fdb673aa75b07e69c
Martin Ly-Ky <martin.ly-ky@gmx.de>
2023-02-23 17:25:26 +0100

    Starter JDBC Projekt

---
+++ src/test/java/codenest/org/jdbctemplate/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package codenest.org.jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
